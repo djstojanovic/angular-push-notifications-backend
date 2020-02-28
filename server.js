0b2531b743df250ccf5cc425f39b3a141057657b
Djordje Stojanovic <55378994+djstojanovic@users.noreply.github.com>
2020-02-28 15:28:05 +0100

    Updated server.js
    Removed personal email.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 // set web push
 webpush.setVapidDetails(
-    'mailto: djordje100janovic@gmail.com',
+    'mailto: <your-email>',
     config.publicKey,
     config.privateKey);
 
